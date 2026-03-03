e375ffb21aac1af8f5198c4159ca3b5c5ba7f6a3
Parth <parth.imscit18@gmail.com>
2026-03-03 22:20:48 +0530

    feat: add seed script to populate initial user and vendor data in the database

--- backend/src/seed.js
+++ backend/src/seed.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const User = require('./models/User');
 const Vendor = require('./models/Vendor');
